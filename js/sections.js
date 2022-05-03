f4710356f3d61bad17d594e47c72afec8d073250
olya <olgamelnyk10111989@gmail.com>
2022-05-03 14:22:40 +0300

    fix

--- js/sections.js
+++ js/sections.js
@@ -24,8 +24,9 @@ $(document).ready(function() {
         if ($(this).val() != '') {
             var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
             if (pattern.test($(this).val())) {
-                $(this).css({ 'border': '1px solid #507A36' });
+                $(this).css({ 'border': '1px solid #507A36', 'color': '' });
                 $('#mail_input').addClass('valid');
+
                 $('#mistake_mail').text('');
             } else {
                 $(this).css({ 'border': '1px solid #DF4E3C' });
